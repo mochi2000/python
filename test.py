ecec0b837446fc09df252c3845efee9f3f1b38b0
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-27 23:04:34 +0900

    条件の追加

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-answer = 6
+import random
+answer = random.randint(1,10)
 
 print("your number?", end="")
 num = int(input())
 
 if answer == num:
     print("Bingo")
+elif answer > num:
+    print("mooo")
 else:
     print("none")
+
+print("[Ansewer: %i]" %answer)
