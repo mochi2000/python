609291549e335c2f4cf1936f90ba7d3391b05c12
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-28 22:11:14 +0900

    while文

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import random
 answer = random.randint(1,10)
 
-print("your number?", end="")
-num = int(input())
 
-if answer == num:
-    print("Bingo")
-elif answer > num:
-    print("mooo")
-else:
-    print("none")
+
+
+while True:
+  print("your number?", end="")
+  num = int(input())
+  
+  if answer == num:
+      print("Bingo")
+      break
+  elif answer > num:
+      print("mooo")
+  else:
+      print("none")
 
 print("[Ansewer: %i]" %answer)
