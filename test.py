40410cbd397d18f0d0cbb0339efcf3dd2f1e7333
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-30 23:06:02 +0900

    コメントとprint

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-import random
-answer = random.randint(1,10)
-count = 0
-
-
-
-
-while True:
-  print("your number?", end="")
-  num = int(input())
-  count += 1
-
-  if answer == num:
-      print("Bingo ok %i" %count )
-      break
-  elif answer > num:
-      print("mooo")
-  else:
-      print("none")
-
-print("[Ansewer: %i]" %answer)
+print("hello")
+#comment
