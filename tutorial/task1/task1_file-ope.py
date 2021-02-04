e30dfb38263679eb9d490a4c9592f1ddd7dceb00
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-02-04 21:46:35 +0900

    ファイルの管理

--- tutorial/task1/task1_file-ope.py
+++ tutorial/task1/task1_file-ope.py
@@ -1,4 +1,10 @@
 import os
 
-print('*** CurrentDirectory ***')
-print(os.getcwd())
+path = './task1/data'
+tmp = os.listdir(path)
+files = []
+for filename in tmp:
+  p = path + '/' + filename
+  if os.path.isfile(p):
+      files.append(filename)
+print(files[:10])
