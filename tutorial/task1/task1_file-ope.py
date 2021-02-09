96441ab0595695725f16b6c522d4dc2facd0c320
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-02-09 22:08:53 +0900

    エクセル

--- tutorial/task1/task1_file-ope.py
+++ tutorial/task1/task1_file-ope.py
@@ -1,16 +1,3 @@
-import os
-import shutil
-import datetime
+import glob
 
-path = './task1/data'
-tmp = os.listdir(path)
-
-for filename in files:
-  from_path = path + '/' + filename
-  d = os.path.getctime(from_path)
-  dd = datetime.date.fromtimestamp(d)
-  year = str(dd.year)
-  to_path = path + '/' + year
-  if not os.path.exists(to_path):
-    os.mkdir(to_path)
-  shutil.move(from_path, to_path)
+filelist = glob.glob('./task2/data/*.xlsx')
