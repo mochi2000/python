3696d0f43e7d219f1ab91f81326cada3cb704c9e
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-02-11 23:04:20 +0900

    エクセルファイル生成

--- tutorial/task1/task1_file-ope.py
+++ tutorial/task1/task1_file-ope.py
@@ -1,4 +1,17 @@
 import glob
 
 filelist = glob.glob('./task2/data/*.xlsx')
-print(filelist)
+
+import openpyx1
+
+out_workbook = openpyx1.Workbook()
+out_sheet = out_workbook.active
+out_sheet.title = '月報一覧'
+
+out_sheet['A1'].value = '報告日'
+out_sheet['B1'].value = '支店'
+out_sheet['C1'].value = '担当者'
+out_sheet['D1'].value = '売り上げ目標(千円)'
+out_sheet['E1'].value = '売り上げ実績(千円)'
+out_sheet['F1'].value = '差異(千円)'
+out_sheet['G1'].value = '達成率(%)'
